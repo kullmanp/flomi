76fb1c217ed44a9f7d16b1144d89058e5a7981f5
Peter Kullmann <p.kullmann@arenae.ch>
2014-01-20 16:43:45 +0100

    Vollständiger Zyklus - REST, JTA Transaction, JPA DB Access

--- ch.kup.flomi.rest/src/ch/kup/flomi/rest/FlomiResource.java
+++ ch.kup.flomi.rest/src/ch/kup/flomi/rest/FlomiResource.java
@@ -5,16 +5,31 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import ch.kup.flomi.integration.FlomiRepository;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Reference;
 
 @Path("flomi")
 @Component(provide=Object.class)
 public class FlomiResource {
-
+	private FlomiRepository flomiRepository;
+	
+	
 	@GET
-	@Produces(MediaType.TEXT_PLAIN)
+	@Produces(MediaType.APPLICATION_JSON)
 	public String sayHello() {
-		return "HELLLO REST WORLD";
+		return "HELLLO REST WORLD" + flomiRepository.findAll();
+	}
+
+
+	public FlomiRepository getFlomiRepository() {
+		return flomiRepository;
+	}
+
+
+	@Reference
+	public void setFlomiRepository(FlomiRepository flomiRepository) {
+		this.flomiRepository = flomiRepository;
 	}
 	
 }
